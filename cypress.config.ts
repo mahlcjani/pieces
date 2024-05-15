9e9b061eee5f7e7dbde399f2ac4f383ffa5c6ed9
Maciej Misiołek <mahlcjani@proton.me>
2024-05-15 12:09:08 +0200

    uncaught exception

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
 //https://github.com/you54f/cypress-multi-reporters
   e2e: {
     baseUrl: "http://localhost:3000",
-    supportFile: false,
+    supportFile: "cypress/support/e2e.ts",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
