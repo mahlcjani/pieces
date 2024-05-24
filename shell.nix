74d5495d8f5701510f56dd44a9266d2486e6ba20
Maciej Misiołek <mahlcjani@proton.me>
2024-05-24 09:30:25 +0200

    Jobs merged into one pipeline.

--- shell.nix
+++ shell.nix
@@ -3,11 +3,8 @@ with import <nixpkgs> {};
 mkShellNoCC {
     packages = [
         act
-        git
         k6
         nodejs
-        openssh
-        vim
     ];
     shellHook = ''
         echo Updating packages...
