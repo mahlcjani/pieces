d908ab840e486a23e5761307c28b9699b4b26cab
Maciej Misiołek <mahlcjani@proton.me>
2024-05-31 12:55:43 +0200

    More reliabel pipeline

--- src/lib/actions/calendar.ts
+++ src/lib/actions/calendar.ts
@@ -9,7 +9,8 @@ import dayjs from "../dayjs";
 import {
   type RecordShape,
   type QueryResult,
-  getSession,
+  Session,
+  getSession
 } from "../neo4j";
 
 import { parsePersonNode } from "./support";
@@ -26,8 +27,6 @@ import { unstable_noStore as noStore } from "next/cache";
 export async function fetchEvents(since: string|Date, until: string|Date = since) {
   noStore();
 
-  // make one or two searches depending on date
-
   const start = dayjs(since);
   const end = dayjs(until);
 
@@ -41,84 +40,16 @@ export async function fetchEvents(since: string|Date, until: string|Date = since
     throw "to wide range"
   }
 
-  if (end.year() > start.year()) {
-    // 2 searches
-    const split = dayjs(new Date(end.year(), 0, 1));
-    console.log(`${start}-${split}`)
-    console.log(`${split}-${end}`)
-
-
-    return (await fetchEvents_(start, split)).concat(await fetchEvents_(split, end));
-
-  } else {
-    console.log(`${start}-${end}`)
-    return await fetchEvents_(start, end);
-
-  }
-
-
-
-}
-
-async function fetchEvents_(since: dayjs.Dayjs, until: dayjs.Dayjs) {
-
-
   const session = await getSession();
 
   try {
-    const result: QueryResult<RecordShape<string, any>> = await session.executeRead(tx =>
-      tx.run(`
-      CALL {
-        WITH date($start) AS start, date($end) AS end
-        MATCH (w:Woman)-[m:IS_MARRIED_TO]-(p)
-        WHERE m.beginDate IS NOT NULL
-          AND start <= date({year: start.year, month: m.beginDate.month, day: m.beginDate.day}) <= end
-        WITH *, {person: w} AS wife, {person: p} AS husband, date({year: start.year, month: m.beginDate.month, day: m.beginDate.day}) AS eventDate
-        RETURN "Marriage" AS eventType,
-            eventDate,
-            duration.between(m.beginDate, eventDate).years AS anniversary,
-            [wife, husband] AS people
-        UNION
-        WITH date($start) AS start, date($end) AS end
-        MATCH (p:Person)
-        WHERE p.birthDate IS NOT NULL
-          AND start <= date({year: start.year, month: p.birthDate.month, day: p.birthDate.day}) <= end
-        WITH *, {person: p} AS person, date({year: start.year, month: p.birthDate.month, day: p.birthDate.day}) AS eventDate
-        RETURN "Birthday" AS eventType,
-          eventDate,
-          duration.between(p.birthDate, eventDate).years AS anniversary,
-          [person] AS people
-        UNION
-        WITH date($start) AS start, date($end) AS end
-        MATCH (p:Person)
-        WHERE start <= date({year: start.year, month: p.nameDate.month, day: p.nameDate.day}) <= end
-        WITH *, {person: p} AS person, date({year: start.year, month: p.nameDate.month, day: p.nameDate.day}) AS eventDate
-        RETURN "Nameday" AS eventType,
-          eventDate,
-          0 AS anniversary,
-          [person] AS people
-      }
-      RETURN *
-      ORDER BY eventDate
-      `,
-        {
-          start: since.format("YYYY-MM-DD"),
-          end: until.format("YYYY-MM-DD")
-        }
-      )
-    );
-
-    console.log(result);
-    console.log(result.records);
-
-    return result.records.map(row => {
-      return {
-        date: dayjs(row.get("eventDate").toStandardDate()).format("YYYY-MM-DD"),
-        type: row.get("eventType"),
-        cardinality: row.get("anniversary").toNumber(),
-        people: row.get("people").map((element: RecordShape) => parsePersonNode(element.person))
-      }
-    });
+    if (end.year() > start.year()) {
+      const split = dayjs(new Date(end.year(), 0, 1));
+      return (await internallFetchEvents(session, start, split)).concat(
+        await internallFetchEvents(session, split, end));
+    } else {
+      return await internallFetchEvents(session, start, end);
+    }
   }
   catch (e) {
     console.log(e);
@@ -128,4 +59,60 @@ async function fetchEvents_(since: dayjs.Dayjs, until: dayjs.Dayjs) {
   }
 
   return [];
+}
+
+async function internallFetchEvents(session: Session, since: dayjs.Dayjs, until: dayjs.Dayjs) {
+  const result: QueryResult<RecordShape<string, any>> = await session.executeRead(tx =>
+    tx.run(`
+    CALL {
+      WITH date($start) AS start, date($end) AS end
+      MATCH (w:Woman)-[m:IS_MARRIED_TO]-(p)
+      WHERE m.beginDate IS NOT NULL
+        AND start <= date({year: start.year, month: m.beginDate.month, day: m.beginDate.day}) <= end
+      WITH *, {person: w} AS wife, {person: p} AS husband, date({year: start.year, month: m.beginDate.month, day: m.beginDate.day}) AS eventDate
+      RETURN "Marriage" AS eventType,
+          eventDate,
+          duration.between(m.beginDate, eventDate).years AS anniversary,
+          [wife, husband] AS people
+      UNION
+      WITH date($start) AS start, date($end) AS end
+      MATCH (p:Person)
+      WHERE p.birthDate IS NOT NULL
+        AND start <= date({year: start.year, month: p.birthDate.month, day: p.birthDate.day}) <= end
+      WITH *, {person: p} AS person, date({year: start.year, month: p.birthDate.month, day: p.birthDate.day}) AS eventDate
+      RETURN "Birthday" AS eventType,
+        eventDate,
+        duration.between(p.birthDate, eventDate).years AS anniversary,
+        [person] AS people
+      UNION
+      WITH date($start) AS start, date($end) AS end
+      MATCH (p:Person)
+      WHERE start <= date({year: start.year, month: p.nameDate.month, day: p.nameDate.day}) <= end
+      WITH *, {person: p} AS person, date({year: start.year, month: p.nameDate.month, day: p.nameDate.day}) AS eventDate
+      RETURN "Nameday" AS eventType,
+        eventDate,
+        0 AS anniversary,
+        [person] AS people
+    }
+    RETURN *
+    ORDER BY eventDate
+    `,
+      {
+        start: since.format("YYYY-MM-DD"),
+        end: until.format("YYYY-MM-DD")
+      }
+    )
+  );
+
+  console.log(result);
+  console.log(result.records);
+
+  return result.records.map(row => {
+    return {
+      date: dayjs(row.get("eventDate").toStandardDate()).format("YYYY-MM-DD"),
+      type: row.get("eventType"),
+      cardinality: row.get("anniversary").toNumber(),
+      people: row.get("people").map((element: RecordShape) => parsePersonNode(element.person))
+    }
+  });
 }
